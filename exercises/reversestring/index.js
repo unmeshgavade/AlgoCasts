a6a9294d965aa52ebaa3480ce9dbcfb6e3246753
Unmesh Gavade <unmeshgavade@gmail.com>
2020-01-13 06:04:11 +0530

    changes

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+// function reverse(str){
+//     return str.split('').reduce((rev, char) => char + rev, '')
+// }
+
+function reverse(str){
+    let reverse = '';
+    for(let char of str){
+        reverse = char + reverse
+    }
+    return reverse;
+}
 
 module.exports = reverse;
