a5b7c4b6843676ea665646570225b4235e0b6b50
Unmesh Gavade <unmeshgavade@gmail.com>
2020-03-01 03:28:47 +0530

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+    const aCharMap = buildCharMaps(stringA)
+    const bCharMap = buildCharMaps(stringB)
+
+    if(Object.keys(aCharMap).length != Object.keys(bCharMap).length){
+        return false
+    }
+
+    for(let char in aCharMap){
+        if(aCharMap[char] != bCharMap[char]){
+            return false
+        }
+    }
+
+    return true
+
+}
+
+function buildCharMaps(str){
+    let charMap = {}
+
+    for(let char of str.replace(/[^\w]/g, "").toLowerCase()){
+        charMap[char] = charMap[char] + 1 || 1;
+    } 
+
+    return charMap
+}
 
 module.exports = anagrams;
